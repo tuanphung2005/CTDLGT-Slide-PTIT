207ab047049994beb3f9ecf69c7f7c85ca05cb6e
yhari <tuan29102005@gmail.com>
2025-09-09 22:03:03 +0700

    commit script fix

--- gitpush.bat
+++ gitpush.bat
@@ -1,21 +1,38 @@
 @echo off
 setlocal enabledelayedexpansion
-set "msg="
-for /f "delims=" %%f in ('git status --porcelain') do (
-    set "line=%%f"
-        set "file=!line:~3!"
-    ) else (
-        set "file=!line:~3!"
-    )
-    echo !file! | findstr /E ".py" >nul
-    if not errorlevel 1 (
-        set "name=!file:.py=!"
-        set "msg=!msg! !name!"
+
+for /f "delims=" %%G in ('where git 2^>nul') do (
+    set "GIT=%%G"
+    goto :foundGit
+)
+
+:foundGit
+
+set "commitMsg="
+set "filesToAdd="
+
+for /f "delims=" %%f in ('"%GIT%" ls-files -o --exclude-standard') do (
+    set "path=%%f"
+    if /I "!path:~-3!"==".py" (
+        for %%A in ("!path!") do (
+            set "base=%%~nA"
+            if defined commitMsg (
+                set "commitMsg=!commitMsg!,!base!"
+            ) else (
+                set "commitMsg=!base!"
+            )
+        )
+        set "filesToAdd=!filesToAdd! "%%f""
     )
 )
-if not defined msg (
-    echo where .py man where.
-    exit /b
+
+if not defined commitMsg (
+    echo No new .py files to commit.
+    exit /b 0
 )
-git add .
-git commit -m "!msg:~1!"
+
+echo Adding: !filesToAdd!
+"%GIT%" add !filesToAdd!
+"%GIT%" commit -m "!commitMsg!"
+
+endlocal
