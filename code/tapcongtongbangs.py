f3ab04489088cc97ed864b0ff3d4545728600069
yhari <tuan29102005@gmail.com>
2025-10-09 03:09:10 +0900

    valid-sudoku,tapcongtongbangs

---
+++ code/tapcongtongbangs.py
@@ -0,0 +1,25 @@
+def check(A, S):
+    dp = [False] * (S + 1)
+    dp[0] = True
+    # print(dp)
+
+    for a in A:
+        print('a=', a)
+        for s in range(S, a - 1, -1):
+            if dp[s - a] == True:
+                dp[s] = True
+            # print('s=',s)
+            # print(dp)
+    
+    return dp[S]
+
+
+T = int(input().strip())
+for _ in range(T):
+    N, S = map(int, input().split())
+    A = list(map(int, input().split()))
+
+    if check(A, S):
+        print("YES")
+    else:
+        print("NO")
